b248dc8b47ce467ef3bfe61cd8ff32286c505c72
madzadev <madars.biss@gmail.com>
2021-12-11 23:45:40 +0200

    edit workout timer

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
 
       <StartWorkoutButton />
 
-      <p>Create a custom workout</p>
+      <p style={{ color: "purple" }}>Create a custom workout</p>
     </div>
   );
 }
