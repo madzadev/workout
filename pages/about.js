4c4625220026fcec9c7b76dde8476986fec68497
madzadev <madars.biss@gmail.com>
2021-12-12 16:54:08 +0200

    edit about

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,15 @@
-<p>
-  Note that created presets are general. It is always recommended do go to the
-  doctor first or arrange a consultation with the exercise specialist to get
-  more special program specific to you.
-</p>;
+const About = () => {
+  return (
+    <div>
+      <h1>Note about programs</h1>
+      <p>
+        Note that created presets are general. It is always recommended do go to
+        the doctor first or arrange a consultation with the exercise specialist
+        to get more special program specific to you.
+      </p>
+      ;
+    </div>
+  );
+};
+
+export default About;
