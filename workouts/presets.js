f6f72c2409ea6222240e47056d944c3a36276257
madzadev <madars.biss@gmail.com>
2021-12-12 16:44:40 +0200

    add workout data

---
+++ workouts/presets.js
@@ -0,0 +1,24 @@
+const workouts = {
+  preset1: {
+    name: "wewe",
+    description: "fdfdf",
+    equipment: "none",
+    exercises: [
+      {
+        name: "sdsdsd",
+        description: "sderfefefesd",
+        length: 23,
+        sdsd: "šdsdads",
+      },
+      {
+        name: "sdsdsd",
+        description: "sderfefefesd",
+        length: 23,
+        sdsd: "šdsdads",
+      },
+    ],
+  },
+  preset2: {},
+};
+
+export default workouts;
