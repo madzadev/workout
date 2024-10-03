1106e2c52a41949b9c79b23f78b75900e8093e8c
madzadev <madars.biss@gmail.com>
2024-10-03 23:53:47 +0300

    edit start button

--- components/StartWorkoutButton.js
+++ components/StartWorkoutButton.js
@@ -2,9 +2,11 @@ import styles from "./StartWorkoutButton.module.css";
 
 const StartWorkoutButton = () => {
   return (
-    <div className={styles.wrapper}>
-      <p>Start</p>
-    </div>
+    <a href="/workout" className={styles.link}>
+      <div className={styles.wrapper}>
+        <p>Start</p>
+      </div>
+    </a>
   );
 };
 
