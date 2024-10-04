705ea26b630655a4a5ff65348161f1e544767420
madzadev <madars.biss@gmail.com>
2024-10-04 22:25:46 +0300

    add progress bar

---
+++ components/ProgressBar.js
@@ -0,0 +1,11 @@
+import styles from "./ProgressBar.module.css";
+
+const StartWorkoutButton = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>The progress of the workout goes here</h1>
+    </div>
+  );
+};
+
+export default StartWorkoutButton;
