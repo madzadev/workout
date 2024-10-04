1ab2a499d23b8a27d3b47c14cb63dcc927ddf124
madzadev <madars.biss@gmail.com>
2024-10-04 23:05:59 +0300

    create workout progress

---
+++ components/WorkoutProgress.js
@@ -0,0 +1,11 @@
+import styles from "./WorkoutProgress.module.css";
+
+const WorkoutProgress = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>The progress of the exercise goes here</h1>
+    </div>
+  );
+};
+
+export default WorkoutProgress;
