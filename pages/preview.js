fb2ad7a49cc39d86d8c2606a0ac447a72d4bb957
madzadev <madars.biss@gmail.com>
2024-10-18 14:27:59 +0300

    add preview page

---
+++ pages/preview.js
@@ -0,0 +1,15 @@
+import Wrapper from "../components/Wrapper";
+import Navigation from "../components/Navigation";
+
+const Preview = () => {
+  return (
+    <Wrapper>
+      <Navigation />
+      <h1>Workout name: AAAAAA</h1>
+      <h3>Workout description: AAAAAAAAA</h3>
+      <h3>Targeted body parts: AAA, BBB, CCC</h3>
+    </Wrapper>
+  );
+};
+
+export default Preview;
