b469b315d4f140158197dbf8fe62b902808ec291
madzadev <madars.biss@gmail.com>
2024-10-18 16:01:19 +0300

    style preview

--- pages/preview.js
+++ pages/preview.js
@@ -1,24 +1,36 @@
 import Wrapper from "../components/Wrapper";
 import Navigation from "../components/Navigation";
+import StartWorkoutButton from "../components/StartWorkoutButton";
+import styles from "../styles/Preview.module.css";
 
 const Preview = () => {
   return (
     <Wrapper>
       <Navigation />
-      <div>
+      <div className={styles.columnWrapper}>
         <div>
-          <h1>Workout name: AAAAAA</h1>
+          <h1 className={styles.title}>Workout name: AAAAAA</h1>
           <h3>Workout description: AAAAAAAAA</h3>
           <h3>Targeted body parts: AAA, BBB, CCC</h3>
           <h3>Equipment needed: none</h3>
         </div>
         <div>
-          <h1>Audio Settings:</h1>
+          <h1 className={styles.title}>Audio Settings:</h1>
           <h3>Music: on</h3>
           <h3>Voiceover: on</h3>
           <h3>Effects: on</h3>
         </div>
       </div>
+      <h1 className={styles.title}>Workout progress</h1>
+      <div className={styles.columnWrapper}>
+        <div>
+          <h3>Lunges</h3>
+        </div>
+        <div>
+          <h3>Do a pushup on the floor and jump up.</h3>
+        </div>
+      </div>
+      <StartWorkoutButton />
     </Wrapper>
   );
 };
