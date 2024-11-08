7b988ef84800b034efe7b3d0ddfb72c7c16aaf60
madzadev <madars.biss@gmail.com>
2024-11-08 17:44:09 +0200

    update navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
     <div className={styles.wrapper}>
       <div className={styles.logo}>
         <Link href="/">
-          <h1 className={styles.links}>💪 FitDevs</h1>
+          <h1 className={styles.links}>💪 FitHub - Devmile</h1>
         </Link>
         {/* <img src="" alt="My logo" /> */}
       </div>
